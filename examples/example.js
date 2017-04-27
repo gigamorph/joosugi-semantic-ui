bf770d5c392d30228a1d2ff5dfc985a67276ab2c
Seong-June Kim <seong-june.kim@yale.edu>
2017-04-27 14:22:18 -0400

    Fix example code after upgrade to webpack 2 and updates on dependencies

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,23 @@
 import AnnotationExplorerDialog from '../src/js/annotation-explorer-dialog';
 import AnnotationSource from './annotation-source';
+import importPackage from '../src/js/import';
+const AnnotationExplorer = importPackage('joosugi').AnnotationExplorer;
+
+const logger = {
+  debug: function(...args) { this.log('DEBUG', args); },
+  info: function(...args) { this.log('INFO', args); },
+  error: function(...args) { this.log('ERROR', args); },
+  log: function(flag, args) {
+    const newArgs = [flag].concat(args);
+    console.log.apply(console, newArgs);
+  }
+};
 
 jQuery(document).ready(function() {
   let layers, canvasRanges, annotations;
-  
+
   const p1 = getCanvasRanges();
-  
+
   Promise.all([p1]).then(function(values) {
     const options = {
       canvases: values[0]
@@ -14,17 +26,24 @@ jQuery(document).ready(function() {
   });
 });
 
-function explore(options) {
+async function explore(options) {
   const annoSource = new AnnotationSource();
+  const annotationExplorer = new AnnotationExplorer({
+    dataSource: annoSource,
+    logger: logger
+  });
+  const layers = await annotationExplorer.getLayers();
   const explorerDialog = new AnnotationExplorerDialog({
     appendTo: jQuery('body'),
-    dataSource: annoSource,
+    annotationExplorer: annotationExplorer,
     canvases: options.canvases,
-    defaultCanvas: 'http://example.org/canvas/01/01',
-    defaultLayer: 'http://example.org/layer/transcription',
+    defaultCanvasId: 'http://example.org/canvas/01/01',
+    layers: layers,
+    defaultLayerId: 'http://example.org/layer/transcription',
     onSelect: function(annotation) {
       jQuery('#output').text(JSON.stringify(annotation, null, 2));
-    }
+    },
+    logger: logger
   });
   jQuery('#open').click(function() {
     jQuery('#output').empty();
